024e078bf8ffd64ffb854ab00c8d2fea4d16dff2
kenneth <kenneth@compass-interactive.com>
2019-06-08 23:01:24 +0800

    test ci

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">View Note</router-link> |
+      <router-link to="/">View Notes</router-link> |
       <router-link to="/about">Insert Note</router-link>
     </div>
     <router-view/>
